84efcf15151b24a6c4b97a07830835041db42179
Adam Whittingham <adam.whittingham@gmail.com>
2015-04-13 11:52:01 +0100

    Add #repos_per_language to count language instances

--- lib/github_favelang.rb
+++ lib/github_favelang.rb
@@ -3,13 +3,21 @@ require 'octokit'
 class GithubFaveLang
 
   def favoute_language username
-    repo_urls = repos_for_user username
+    repos = repos_for_user username
+    repos_per_language(repos)
   end
 
   def repos_for_user username
     user = Octokit.user(username)
     user_repos = user.rels[:repos].get
-    user_repos.data.map{|repo| repo[:html_url]}
+    user_repos.data
+  end
+
+  def repos_per_language repos
+    repos
+      .map{|repo| repo[:language]}
+      .compact
+      .each_with_object(Hash.new(0)) { |lang, counts| counts[lang] += 1 }
   end
 
 end
--- spec/github_favelang_spec.rb
+++ spec/github_favelang_spec.rb
@@ -2,14 +2,29 @@ require 'github_favelang'
 
 describe GithubFaveLang do
   let(:user){ 'AdamWhittingham' }
+  let(:vim_repo) { "https://github.com/#{user}/vim-config" }
 
   describe '#repos_for_user' do
-    it 'returns repo urls for a given user' do
-      expect(subject.repos_for_user(user)).to include "https://github.com/#{user}/vim-config"
+    let(:repos) { subject.repos_for_user(user) }
+
+    it 'returns repos for a given user' do
+      expect(repos.map{|r| r.html_url}).to include vim_repo
+    end
+
+  end
+
+  describe '#repos_per_language' do
+    let (:vim_repo)  { {language: 'vim'} }
+    let (:ruby_repo) { {language: 'ruby'} }
+    let (:nil_repo)  { {language: nil} }
+    let (:example_input) {
+      [ vim_repo, ruby_repo, nil_repo, ruby_repo ]
+    }
+
+    it 'returns the count each language was seen' do
+      result = subject.repos_per_language(example_input)
+      expect(result).to eq({'ruby' => 2, 'vim' => 1})
     end
   end
 
-  describe '#repo_main_language' do
-    it 'reutns the main language for a repository'
-  end
 end
